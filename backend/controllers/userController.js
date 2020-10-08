aa1968e9dfac55b5368e928ca20ce3b7e91dfb5d
Elango <elango@Elangos-MacBook-Pro.local>
2020-10-08 22:01:10 +0530

    39-generate_json_web_token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 import asyncHandler from "express-async-handler";
 
 // @desc Get and auth user
@@ -15,7 +16,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
