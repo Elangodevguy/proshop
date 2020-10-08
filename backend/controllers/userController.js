bfc3203f3d3e471887f3ae487f918a3807f13b0d
Elango <elango@Elangos-MacBook-Pro.local>
2020-10-08 23:30:08 +0530

    42-user_registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,8 +2,41 @@ import User from "../models/userModel.js";
 import generateToken from "../utils/generateToken.js";
 import asyncHandler from "express-async-handler";
 
-// @desc Get and auth user
-// @route GET /api/users/login
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+// @desc POST and auth user
+// @route POST /api/users/login
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -43,4 +76,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+export { authUser, getUserProfile, registerUser };
